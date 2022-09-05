c44666c5ef9e2d5a962bc216d790bf06f9a10901
Combatxd <logan.schoerner@gmail.com>
2022-09-05 12:04:32 -0600

    initalCommit

---
+++ server.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'company_db'
+    },
+);
+
+
