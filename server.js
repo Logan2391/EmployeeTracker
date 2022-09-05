2bdfce3b838692287b06dd8f3afedec2d93d859f
Combatxd <logan.schoerner@gmail.com>
2022-09-05 14:52:27 -0600

    first working function

--- server.js
+++ server.js
@@ -11,6 +11,91 @@ const db = mysql.createConnection(
 );
 
 const promptUser = () => {
-    
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: "choices",
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ]
+        }
+    ])
+        .then((answers) => {
+            const { choices } = answers;
+
+            if (choices === "View all departments") {
+                viewDepartments();
+            }
+
+            if (choices === "View all roles") {
+                viewRoles();
+            }
+
+            if (choices === "View all employees") {
+                viewEmployees();
+            }
+
+            if (choices === "Add a department") {
+                addDepartment();
+            }
+
+            if (choices === "Add a role") {
+                addRole();
+            }
+
+            if (choices === "Add an employee") {
+                addEmployee();
+            }
+
+            if (choices === "Update an employee role") {
+                updateEmployeeRole();
+            }
+        });
+};
+
+const viewDepartments = () => {
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+
+    db.promise().query(sql)
+        .then(([rows]) => {
+            console.log(rows);
+            promptUser();
+        })
+        .catch(console.log)
+};
+
+const viewRoles = () => {
+
 }
 
+const viewEmployees = () => {
+
+}
+
+const addDepartment = () => {
+
+}
+
+const addRole = () => {
+
+}
+
+const addEmployee = () => {
+
+}
+
+const updateEmployeeRole = () => {
+
+}
+
+
+
+
+promptUser();
